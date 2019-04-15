7cbe8a0cb38c2e4ade1d3a6c9d52f27ed4941e46
booldook <booldook@gmail.com>
2019-04-15 13:36:58 +0900

    google signin

--- public/js/auth.js
+++ public/js/auth.js
@@ -2,6 +2,15 @@ var auth = firebase.auth();
 var googleAuth = new firebase.auth.GoogleAuthProvider();
 
 $("#bt_login_google").click(function(){
-	//auth.signInWithPopup(googleAuth);
-	auth.signInWithRedirect(googleAuth);
+	auth.signInWithPopup(googleAuth);
+	//auth.signInWithRedirect(googleAuth);
+});
+$("#bt_logout_google").click(function(){
+	auth.signOut();
+});
+auth.onAuthStateChanged(function(data){
+	console.log(data.displayName);
+	console.log(data.email);
+	console.log(data.photoURL);
+	console.log(data.uid);
 });
