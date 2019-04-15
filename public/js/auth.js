b7f4e8e85273731810c993ce9753857ac1d27b43
booldook <booldook@gmail.com>
2019-04-15 14:00:18 +0900

    auth

--- public/js/auth.js
+++ public/js/auth.js
@@ -9,8 +9,21 @@ $("#bt_logout_google").click(function(){
 	auth.signOut();
 });
 auth.onAuthStateChanged(function(data){
-	console.log(data.displayName);
-	console.log(data.email);
-	console.log(data.photoURL);
-	console.log(data.uid);
+	if(data == null) {
+		$(".google_icon").children("img").attr("src", "");
+		$(".google_cont").children("div").html("");
+		$(".google_icon").hide();
+		$(".google_cont").hide();
+		$("#bt_login_google").show();
+		$("#bt_logout_google").hide();
+	}
+	else {
+		$(".google_icon").children("img").attr("src", data.photoURL);
+		$(".google_cont").children("div").eq(0).html(data.displayName);
+		$(".google_cont").children("div").eq(1).html(data.email);
+		$(".google_icon").show();
+		$(".google_cont").show();
+		$("#bt_login_google").hide();
+		$("#bt_logout_google").show();
+	}
 });
