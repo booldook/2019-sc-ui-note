a1c3a320f7fc051b0e492a2651b99b5e23096321
booldook <booldook@gmail.com>
2019-04-16 10:56:20 +0900

    gbook

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,16 +1,52 @@
-var config = {
+// firebase init
+firebase.initializeApp({
 	apiKey: "AIzaSyBdqHr1B8-GGa1UpavRBGo2wfDMSTpXFuI",
 	authDomain: "booldook-note3.firebaseapp.com",
 	databaseURL: "https://booldook-note3.firebaseio.com",
 	projectId: "booldook-note3",
 	storageBucket: "booldook-note3.appspot.com",
 	messagingSenderId: "59442077127"
-};
-firebase.initializeApp(config);
+});
 
+// 전역변수 설정
 var auth = firebase.auth();
 var googleAuth = new firebase.auth.GoogleAuthProvider();
+var db = firebase.database();
+var ref = null;
 
+// 최초실행함수
+(function init(){
+	db.ref("root/users").on("child_added", onAdd);
+	db.ref("root/users").on("child_removed", onRev);
+	db.ref("root/users").on("child_changed", onChg);
+})();
+
+// DB 저장
+$("#bt_join").click(function(){
+	db.ref("root/users").push({
+		name: $("#name").val(),
+		email: $("#email").val(),
+		pass: $("#pass").val()
+	}).key;
+});
+
+// DB 이벤트 콜백
+function onAdd(data) {
+	console.log(data);
+}
+
+function onChg(data) {
+	console.log(data);
+}
+
+function onRev(data) {
+	console.log(data);
+}
+
+
+
+
+// auth 로그인/로그아웃 이벤트
 $("#bt_login_google").click(function(){
 	auth.signInWithPopup(googleAuth);
 	//auth.signInWithRedirect(googleAuth);
@@ -18,6 +54,8 @@ $("#bt_login_google").click(function(){
 $("#bt_logout_google").click(function(){
 	auth.signOut();
 });
+
+// auth 이벤트 콜백
 auth.onAuthStateChanged(function(data){
 	if(data == null) {
 		$(".google_icon").children("img").attr("src", "");
