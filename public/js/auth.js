f742583e7504e017fef9a5572581a398db8f7732
booldook <booldook@gmail.com>
2019-04-16 10:19:46 +0900

    db 시작

--- public/js/auth.js
+++ public/js/auth.js
@@ -1,3 +1,13 @@
+var config = {
+	apiKey: "AIzaSyBdqHr1B8-GGa1UpavRBGo2wfDMSTpXFuI",
+	authDomain: "booldook-note3.firebaseapp.com",
+	databaseURL: "https://booldook-note3.firebaseio.com",
+	projectId: "booldook-note3",
+	storageBucket: "booldook-note3.appspot.com",
+	messagingSenderId: "59442077127"
+};
+firebase.initializeApp(config);
+
 var auth = firebase.auth();
 var googleAuth = new firebase.auth.GoogleAuthProvider();
 
