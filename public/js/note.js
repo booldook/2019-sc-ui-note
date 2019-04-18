5da08adfe77bc43867c1871912595c4221d38d4d
booldook <booldook@gmail.com>
2019-04-18 11:59:23 +0900

    chg

--- public/js/note.js
+++ public/js/note.js
@@ -35,30 +35,10 @@ function zp(n) {
 /***** 인증처리 *****/
 auth.onAuthStateChanged(onAuth);
 function onAuth(data) {
-	$(".lists").empty();
 	user = data;
-	if(data == null) {
-		$(".signs > .photos").hide();
-		$(".signs > .conts").hide();
-		$("#bt_signin").show();
-		$("#bt_signout").hide();
-		$(".signs > .photos > img").attr("src", "");
-		$(".signs > .conts > div").html("");
-		$("#bt_modal_close").hide();
-		$("#bt_signin2").show();
-		modalOpen("알림", "로그인을 하셔야 사이트를 이용하실 수 있습니다.<br>하단의 구글 로그인을 이용하세요.");
-	}
+	if(user == null) chgState('');
 	else {
-		$(".signs > .photos").show();
-		$(".signs > .conts").show();
-		$("#bt_signin").hide();
-		$("#bt_signout").show();
-		$(".signs > .photos > img").attr("src", data.photoURL);
-		$(".signs > .conts > div").eq(0).text(data.displayName);
-		$(".signs > .conts > div").eq(1).text(data.email);
-		$("#bt_modal_close").show();
-		$("#bt_signin2").hide();
-		$("#bt_modal_close").trigger("click");
+		chgState('R');
 		dbInit();
 	}
 }
@@ -82,7 +62,7 @@ function onAdd(data) {
 	var bTit = data.val().content.substring(0, 1);
 	var colors = ["cornflowerblue",	"darkcyan", "darkorange", "deeppink","steelblue"];
 	var color = colors[Math.floor(Math.random()*5)];
-	html += '<li class="list" id="'+data.key+'">';
+	html += '<li class="list" id="'+data.key+'" onclick="dataChg(this);">';
 	html += '<h1 style="background-color:'+color+'">'+bTit+'</h1>';
 	html += '<div>';
 	html += '<div class="title">'+data.val().content+'</div>';
@@ -103,7 +83,7 @@ function onChg(data) {
 
 /***** 데이터베이스 버튼 이벤트 *****/
 $("#bt_new").on("click", function(){
-
+	chgState('C');
 });
 $("#bt_save").on("click", function(){
 	var content = $("#content").val();
@@ -125,26 +105,71 @@ $("#bt_up").on("click", function(){
 $("#bt_cls").on("click", function(){
 
 });
+$("#content").click(function(){
+	chgState('C');
+});
 function dataRev(obj) {
-	var $li = $(obj).parent().parent()
+	var $li = $(obj).parent().parent();
 	var key = $li.attr("id");
 	db.ref("root/notes/"+user.uid+"/"+key).remove();
 }
+function dataChg(obj) {
+	var $li = $(obj).parent().parent();
+	var key = $li.attr("id");
+}
+
 
 /***** 상태(UI)변경함수 *****/
 function chgState(chk) {
 	switch(chk) {
 		case "C" :
-			//실행문
+			$("#bt_new").attr("disabled", false);
+			$("#bt_save").attr("disabled", false);
+			$("#bt_up").hide();
+			$("#bt_cls").attr("disabled", false);
 			break;
 		case "R" :
-			//실행문
+			$(".lists").empty();
+			$("#bt_new").attr("disabled", false);
+			$("#bt_save").attr("disabled", "disabled");
+			$("#bt_up").hide();
+			$("#bt_cls").attr("disabled", "disabled");
+			$("#content").val('');
+			//로그인UI
+			$(".signs > .photos").show();
+			$(".signs > .conts").show();
+			$("#bt_signin").hide();
+			$("#bt_signout").show();
+			$(".signs > .photos > img").attr("src", user.photoURL);
+			$(".signs > .conts > div").eq(0).text(user.displayName);
+			$(".signs > .conts > div").eq(1).text(user.email);
+			$("#bt_modal_close").show();
+			$("#bt_signin2").hide();
+			$("#bt_modal_close").trigger("click");
 			break;
 		case "U" :
 			//실행문
 			break;
 		case "D" :
 			//실행문
+			break;
+		default :
+			$(".lists").empty();
+			$("#bt_new").attr("disabled", "disabled");
+			$("#bt_save").attr("disabled", "disabled");
+			$("#bt_up").hide();
+			$("#bt_cls").attr("disabled", "disabled");
+			$("#content").val('');
+			//로그인UI
+			$(".signs > .photos").hide();
+			$(".signs > .conts").hide();
+			$("#bt_signin").show();
+			$("#bt_signout").hide();
+			$(".signs > .photos > img").attr("src", "");
+			$(".signs > .conts > div").html("");
+			$("#bt_modal_close").hide();
+			$("#bt_signin2").show();
+			modalOpen("알림", "로그인을 하셔야 사이트를 이용하실 수 있습니다.<br>하단의 구글 로그인을 이용하세요.");
 			break;
 	}
 }
