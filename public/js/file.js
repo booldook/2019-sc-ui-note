3d2dfbe51299f47f9c08eacaba80bd51e20f7a72
booldook <booldook@gmail.com>
2019-04-19 14:54:22 +0900

    upload 완성

--- public/js/file.js
+++ public/js/file.js
@@ -27,6 +27,22 @@ function splitName(name) {
 	obj.newName = obj.newFile + '.' + obj.ext;
 	return obj;
 }
+function localDate(ts) {
+	var d = new Date(ts);
+	var dt = '';
+	//dt += d.getFullYear().toString().substring(2, 4) + '-';
+	dt += d.getFullYear() + '-';
+	dt += zp(d.getMonth() + 1) + '-';
+	dt += zp(d.getDate()) + ' ';
+	dt += zp(d.getHours()) + ':';
+	dt += zp(d.getMinutes()) + ':';
+	dt += zp(d.getSeconds());
+	return dt;
+}
+function zp(n) {
+	if(n < 10) return '0'+n;
+	else return n;
+}
 
 /***** 사용자 인증 *****/
 auth.onAuthStateChanged(function(data){
@@ -47,24 +63,43 @@ function onSave(){
 	var file = $("#pds")[0].files[0];
 	var gFile = splitName(file.name);
 	var data = null;
+	$(".loader").css("display", "flex");
 	var uploader = root.child(gFile.newName).put(file);
 	uploader.on('state_changed', uploadIng, uploadErr, uploadDone);
 	function uploadIng(snapshot) {
 		data = snapshot;
 		var progress = (data.bytesTransferred / data.totalBytes) * 100;
-		console.log('업로드 진행률 : ' + progress + '%');
+		$(".loader > .txt").html(Math.ceil(progress) + '%');
 	}
 	function uploadErr(err) {
-		console.log(err);
+		$(".loader").css("display", "none");
+		modalOpen("경고", "파일업로드에 실패하였습니다.");
 	}
 	function uploadDone() {
+		$(".loader").css("display", "none");
 		data.ref.getDownloadURL().then(function(url) {
-			console.log('파일위치 : ', url);
+			var saveData = {};
+			saveData.content = $("#content").val();
+			saveData.oriName = gFile.oriName;
+			saveData.newName = gFile.newName;
+			saveData.wdate = gFile.time;
+			saveData.size = file.size;
+			saveData.url = url;
+			db.ref("root/uploads/"+user.uid).push(saveData).key;
+			db.ref("root/uploads/"+user.uid).on("child_added", function(data){
+				var html = '';
+				html += '<li>원본파일명 : '+data.val().oriName+'</li>';
+				html += '<li>저장파일명 : '+data.val().newName+'</li>';
+				html += '<li>저장된위치 : <a href="'+data.val().url+'" target="_blank">'+data.val().url+'</a></li>';
+				html += '<li>저장된시간 : '+localDate(data.val().wdate)+'</li>';
+				html += '<li>파일사이즈 : '+data.val().size+'</li>';
+				html += '<li>남긴한마디 : '+data.val().content+'</li>';
+				html += '<li><img src="'+data.val().url+'" class="img"></li>';
+				$(".console").html(html);
+			});
 		});
 	}
 }
-
-
 // uploader.on("state_changed", 함수1, 함수2, 함수3);
 // 함수1: 진행중, 함수2: 진행에러, 함수3: 진행완료
 
